f4558956e64b9f5557aafd66e8dbc7273a812178
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-10-17 15:52:31 -0500

    add function to add tasks

--- todolist/todojs.js
+++ todolist/todojs.js
@@ -1,2 +1,14 @@
-const inputText = document.getElementById("text");
+const text = document.getElementById("text");
 const list = document.getElementById("list");
+
+function addTask() {
+    if(text.value === ''){
+        alert("Cannot be blank!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = text.value;
+        list.appendChild(li);
+    }
+    text.value = "";
+}
