a8586d3389da1ecc7e71c8cdc71ab9c1df647c05
Maria Vo <38661852+mariavo1@users.noreply.github.com>
2023-10-18 13:46:02 -0500

    create span for delete task

--- todolist/todojs.js
+++ todolist/todojs.js
@@ -9,6 +9,9 @@ function addTask() {
         let li = document.createElement("li");
         li.innerHTML = text.value;
         list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
     }
     text.value = "";
 }
